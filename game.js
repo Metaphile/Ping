a815042df1f1f687c14d119a96649c3f9c5cdaea
Adam Siler <lasiler@gmail.com>
2013-11-01 11:05:56 -0500

    No longer using __proto__ property for state inheritance.

--- game.js
+++ game.js
@@ -12,200 +12,220 @@ var GAME = (function () {
 			
 			that.onEnter = ENGINE.noop;
 			
-			states.title = new function () {
-				var that = this;
-				
-				var BLINK_INTERVAL = 0.6, elapsed, dimPrompt;
-				
-				that.onEnter = function () {
-					elapsed = 0;
-				};
-				
-				that.onKeyDown = function (key) {
-					if (key === ENGINE.Keyboard.keys.enter) {
-						currentState = states.serving;
-						currentState.onEnter();
-					}
-				};
-				
-				that.update = function (interval) {
-					elapsed += interval;
-					if (elapsed > BLINK_INTERVAL) {
-						dimPrompt = !dimPrompt;
-						elapsed -= BLINK_INTERVAL;
-					}
-				};
-				
-				that.draw = function () {
-					ctx.textAlign = 'center';
-					ctx.fillStyle = 'white';
-					ctx.font = 'bold 128px monospace';
-					ctx.fillText('Ping', ctx.canvas.width/2, ctx.canvas.height/2);
-					
-					// start prompt
-					ctx.fillStyle = dimPrompt ? 'gray' : 'white';
-					ctx.font = 'normal 20px monospace';
-					ctx.fillText('Press Start', ctx.canvas.width/2, ctx.canvas.height/2 + 60);
-				};
-			};
-			states.title.__proto__ = that;
-			
-			states.main = new function () {
-				var that = this;
-				var score, spareBalls;
-				var TOTAL_BALLS = 3;
-				var WALL_THICKNESS = 50;
-				
-				var entities = [];
-				var walls = [];
-				var paddles = [];
-				
-				(function () {
-					var upperWall = new ENTITIES.Wall(ctx);
-					upperWall.boundary.left   = -9999;
-					upperWall.boundary.right  = ctx.canvas.width + 9999;
-					upperWall.boundary.top    = -9999;
-					upperWall.boundary.bottom = WALL_THICKNESS;
-					entities.push(upperWall);
-					walls.push(upperWall);
-					
-					var lowerWall = new ENTITIES.Wall(ctx);
-					lowerWall.boundary.left   = -9999;
-					lowerWall.boundary.right  = ctx.canvas.width + 9999;
-					lowerWall.boundary.top    = ctx.canvas.height - WALL_THICKNESS;
-					lowerWall.boundary.bottom = ctx.canvas.height + 9999;
-					entities.push(lowerWall);
-					walls.push(lowerWall);
-				})();
-				
-				(function () {
-					var leftPaddle = new ENTITIES.Paddle(ctx);
-					leftPaddle.position.x = leftPaddle.width/2 + 30;
-					entities.push(leftPaddle);
-					paddles.push(leftPaddle);
-					
-					var rightPaddle = new ENTITIES.Paddle(ctx);
-					rightPaddle.position.x = ctx.canvas.width - rightPaddle.width/2 - 30;
-					entities.push(rightPaddle);
-					paddles.push(rightPaddle);
-				})();
-				
-				var ball = new ENTITIES.Ball(ctx);
-				entities.push(ball);
-				
-				that.onEnter = function () {
-					score = 0;
-					spareBalls = TOTAL_BALLS;
-					// vertically center paddles
-					paddles.forEach(function (paddle) { paddle.position.y = ctx.canvas.height/2; });
-				};
-				
-				that.onMouseMove = function (position) {
-					paddles.forEach(function (paddle) { paddle.position.y = position.y; });
-				};
-				
-				that.update = function (interval) {
-					entities.forEach(function (entity) { entity.update(interval); });
-					
-					// paddle-wall collisions
-					for (var i = paddles.length - 1; i >= 0; i--) {
-						for (var j = walls.length - 1; j >= 0; j--) {
-							var escapeVector = paddles[i].boundary.test(walls[j].boundary);
-							if (escapeVector) {
-								paddles[i].onCollision(walls[j], escapeVector);
-							}
-						}
-					}
-					
-					// ball-wall collisions
-					walls.forEach(function (wall) {
-						var escapeVector = ball.boundary.test(wall.boundary);
-						if (escapeVector) ball.onCollision(wall, escapeVector);
-					});
-					
-					// ball-paddle collisions
-					paddles.forEach(function (paddle) {
-						var escapeVector = ball.boundary.test(paddle.boundary);
-						if (escapeVector) ball.onCollision(paddle, escapeVector);
-					});
-				};
-				
-				that.draw = function () {
-					// score
-					
-					ctx.textAlign = 'center';
-					ctx.fillStyle = 'white';
-					ctx.font = 'bold 30px monospace';
-					ctx.fillText(score, ctx.canvas.width/2, 34);
-					
-					// spare balls (heh)
-					
-					var SYMBOL_RADIUS = 7;
-					var SPACING = 6;
-					var LEFT = ctx.canvas.width - (TOTAL_BALLS * (SYMBOL_RADIUS*2 + SPACING)) + SYMBOL_RADIUS - 100;
-					
-					for (var i = 0; i < TOTAL_BALLS; i++) {
-						ctx.beginPath();
-						ctx.arc(LEFT + i * (SYMBOL_RADIUS*2 + SPACING), WALL_THICKNESS/2, SYMBOL_RADIUS, 0, Math.PI * 2);
-						ctx.fillStyle = (i < spareBalls ? 'white' : 'gray');
-						ctx.fill();
-					}
-					
-					entities.forEach(function (entity) { entity.draw(); });
-				};
-				
-				states.serving = new function () {
+			states.title = (function () {
+				function Title() {
 					var that = this;
 					
-					var DURATION = 3, remaining;
-					var BALL_SPEED = 400;
+					var BLINK_INTERVAL = 0.6, elapsed, dimPrompt;
 					
 					that.onEnter = function () {
-						that.__proto__.onEnter();
-						
-						remaining = DURATION;
-						
-						spareBalls--;
-						
-						ball.position.x = ctx.canvas.width/2;
-						ball.position.y = ctx.canvas.height/2;
+						elapsed = 0;
+					};
+					
+					that.onKeyDown = function (key) {
+						if (key === ENGINE.Keyboard.keys.enter) {
+							currentState = states.serving;
+							currentState.onEnter();
+						}
 					};
 					
 					that.update = function (interval) {
-						that.__proto__.update(interval);
-						
-						remaining -= interval;
-						if (remaining <= 0) {
-							var angle = Math.randRange(-45, 45);
-							// randomly serve to the left or right
-							if (Math.random() > 0.5) { angle += 180; }
-							ball.velocity.x = Math.cos(angle * Math.PI/180) * BALL_SPEED;
-							ball.velocity.y = Math.sin(angle * Math.PI/180) * BALL_SPEED;
-							
-							currentState = states.playing;
-							states.playing.onEnter();
+						elapsed += interval;
+						if (elapsed > BLINK_INTERVAL) {
+							dimPrompt = !dimPrompt;
+							elapsed -= BLINK_INTERVAL;
 						}
 					};
 					
 					that.draw = function () {
-						that.__proto__.draw();
-						
-						ctx.fillStyle = 'white';
-						ctx.font = 'normal 30px monospace';
 						ctx.textAlign = 'center';
-						ctx.fillText(Math.ceil(remaining), ctx.canvas.width/2, ctx.canvas.height/2 - 25);
+						ctx.fillStyle = 'white';
+						ctx.font = 'bold 128px monospace';
+						ctx.fillText('Ping', ctx.canvas.width/2, ctx.canvas.height/2);
+						
+						// start prompt
+						ctx.fillStyle = dimPrompt ? 'gray' : 'white';
+						ctx.font = 'normal 20px monospace';
+						ctx.fillText('Press Start', ctx.canvas.width/2, ctx.canvas.height/2 + 60);
 					};
-				};
-				states.serving.__proto__ = that;
+				}
 				
-				states.playing = new function () {
+				Title.prototype = that; // states.base
+				
+				return new Title();
+			})();
+			
+			states.main = (function () {
+				function Main() {
 					var that = this;
+					var score, spareBalls;
+					var TOTAL_BALLS = 3;
+					var WALL_THICKNESS = 50;
 					
-					that.onEnter = ENGINE.noop;
-				};
-				states.playing.__proto__ = that;
-			};
-			states.main.__proto__ = that;
+					var entities = [];
+					var walls = [];
+					var paddles = [];
+					
+					(function () {
+						var upperWall = new ENTITIES.Wall(ctx);
+						upperWall.boundary.left   = -9999;
+						upperWall.boundary.right  = ctx.canvas.width + 9999;
+						upperWall.boundary.top    = -9999;
+						upperWall.boundary.bottom = WALL_THICKNESS;
+						entities.push(upperWall);
+						walls.push(upperWall);
+						
+						var lowerWall = new ENTITIES.Wall(ctx);
+						lowerWall.boundary.left   = -9999;
+						lowerWall.boundary.right  = ctx.canvas.width + 9999;
+						lowerWall.boundary.top    = ctx.canvas.height - WALL_THICKNESS;
+						lowerWall.boundary.bottom = ctx.canvas.height + 9999;
+						entities.push(lowerWall);
+						walls.push(lowerWall);
+					})();
+					
+					(function () {
+						var leftPaddle = new ENTITIES.Paddle(ctx);
+						leftPaddle.position.x = leftPaddle.width/2 + 30;
+						entities.push(leftPaddle);
+						paddles.push(leftPaddle);
+						
+						var rightPaddle = new ENTITIES.Paddle(ctx);
+						rightPaddle.position.x = ctx.canvas.width - rightPaddle.width/2 - 30;
+						entities.push(rightPaddle);
+						paddles.push(rightPaddle);
+					})();
+					
+					var ball = new ENTITIES.Ball(ctx);
+					entities.push(ball);
+					
+					that.onEnter = function () {
+						score = 0;
+						spareBalls = TOTAL_BALLS;
+						// vertically center paddles
+						paddles.forEach(function (paddle) { paddle.position.y = ctx.canvas.height/2; });
+					};
+					
+					that.onMouseMove = function (position) {
+						paddles.forEach(function (paddle) { paddle.position.y = position.y; });
+					};
+					
+					that.update = function (interval) {
+						entities.forEach(function (entity) { entity.update(interval); });
+						
+						// paddle-wall collisions
+						for (var i = paddles.length - 1; i >= 0; i--) {
+							for (var j = walls.length - 1; j >= 0; j--) {
+								var escapeVector = paddles[i].boundary.test(walls[j].boundary);
+								if (escapeVector) {
+									paddles[i].onCollision(walls[j], escapeVector);
+								}
+							}
+						}
+						
+						// ball-wall collisions
+						walls.forEach(function (wall) {
+							var escapeVector = ball.boundary.test(wall.boundary);
+							if (escapeVector) ball.onCollision(wall, escapeVector);
+						});
+						
+						// ball-paddle collisions
+						paddles.forEach(function (paddle) {
+							var escapeVector = ball.boundary.test(paddle.boundary);
+							if (escapeVector) ball.onCollision(paddle, escapeVector);
+						});
+					};
+					
+					that.draw = function () {
+						// score
+						
+						ctx.textAlign = 'center';
+						ctx.fillStyle = 'white';
+						ctx.font = 'bold 30px monospace';
+						ctx.fillText(score, ctx.canvas.width/2, 34);
+						
+						// spare balls (heh)
+						
+						var SYMBOL_RADIUS = 7;
+						var SPACING = 6;
+						var LEFT = ctx.canvas.width - (TOTAL_BALLS * (SYMBOL_RADIUS*2 + SPACING)) + SYMBOL_RADIUS - 100;
+						
+						for (var i = 0; i < TOTAL_BALLS; i++) {
+							ctx.beginPath();
+							ctx.arc(LEFT + i * (SYMBOL_RADIUS*2 + SPACING), WALL_THICKNESS/2, SYMBOL_RADIUS, 0, Math.PI * 2);
+							ctx.fillStyle = (i < spareBalls ? 'white' : 'gray');
+							ctx.fill();
+						}
+						
+						entities.forEach(function (entity) { entity.draw(); });
+					};
+					
+					states.serving = (function () {
+						function Serving() {
+							var that = this;
+							
+							var DURATION = 3, remaining;
+							var BALL_SPEED = 400;
+							
+							that.onEnter = function () {
+								that.__proto__.onEnter();
+								
+								remaining = DURATION;
+								
+								spareBalls--;
+								
+								ball.position.x = ctx.canvas.width/2;
+								ball.position.y = ctx.canvas.height/2;
+							};
+							
+							that.update = function (interval) {
+								that.__proto__.update(interval);
+								
+								remaining -= interval;
+								if (remaining <= 0) {
+									var angle = Math.randRange(-45, 45);
+									// randomly serve to the left or right
+									if (Math.random() > 0.5) { angle += 180; }
+									ball.velocity.x = Math.cos(angle * Math.PI/180) * BALL_SPEED;
+									ball.velocity.y = Math.sin(angle * Math.PI/180) * BALL_SPEED;
+									
+									currentState = states.playing;
+									states.playing.onEnter();
+								}
+							};
+							
+							that.draw = function () {
+								that.__proto__.draw();
+								
+								ctx.fillStyle = 'white';
+								ctx.font = 'normal 30px monospace';
+								ctx.textAlign = 'center';
+								ctx.fillText(Math.ceil(remaining), ctx.canvas.width/2, ctx.canvas.height/2 - 25);
+							};
+						}
+						
+						Serving.prototype = that; // states.main
+						
+						return new Serving();
+					})();
+					
+					states.playing = (function () {
+						function Playing() {
+							var that = this;
+							
+							that.onEnter = ENGINE.noop;
+						}
+						
+						Playing.prototype = that; // states.main
+						
+						return new Playing();
+					})();
+				}
+				
+				Main.prototype = that; // states.base
+				
+				return new Main();
+			}());
 		};
 		
 		// creates a public method that delegates to the current state
