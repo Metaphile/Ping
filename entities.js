5b08bbd7954fbc7569b8ba566e249c22268ddbaa
Adam Siler <lasiler@gmail.com>
2013-11-21 15:20:09 -0600

    Points now pop up.

--- entities.js
+++ entities.js
@@ -194,18 +194,21 @@ var ENTITIES = (function () {
 		
 		that.value = 0;
 		
-		var FADE_DURATION = 3, fadeDurationElapsed;
+		var FADE_DURATION = 1.7, fadeDurationElapsed;
+		// for pop up animation
+		var verticalOffset;
 		
 		that.initialize = function () {
 			fadeDurationElapsed = 0;
+			verticalOffset = 0;
 		};
 		
 		that.update = function (deltaTime) {
 			if (fadeDurationElapsed < FADE_DURATION) {
 				fadeDurationElapsed += deltaTime;
 				
-				// float up
-				that.position.y -= 2000 * (1 - Math.pow(fadeDurationElapsed/FADE_DURATION, 0.008)) * deltaTime;
+				// pop up
+				verticalOffset = ENGINE.tweens.easeOutElastic(null, fadeDurationElapsed, 0, -20, 1);
 			}
 		};
 		
@@ -216,16 +219,18 @@ var ENTITIES = (function () {
 			var text = '$' + that.value.withCommas();
 			var opacity = 1 - Math.pow(fadeDurationElapsed/FADE_DURATION, 2);
 			
+			// black outline
 			ctx.strokeStyle = 'rgba(0, 0, 0, ' + opacity + ')';
 			ctx.lineWidth = 4;
-			ctx.strokeText(text, that.position.x, that.position.y);
+			ctx.strokeText(text, that.position.x, that.position.y + verticalOffset);
 			
+			// yellow glow
 			ctx.strokeStyle = 'rgba(255, 255, 127, ' + opacity + ')';
 			ctx.lineWidth = 2;
-			ctx.strokeText(text, that.position.x, that.position.y);
+			ctx.strokeText(text, that.position.x, that.position.y + verticalOffset);
 			
 			ctx.fillStyle = 'rgba(255, 255, 255, ' + opacity + ')';
-			ctx.fillText(text, that.position.x, that.position.y);
+			ctx.fillText(text, that.position.x, that.position.y + verticalOffset);
 		};
 		
 		that.initialize();
