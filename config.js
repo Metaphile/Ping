b7f31dbd0a1cc4a4566fc6c97c3b3caae61f77ed
Adam Siler <lasiler@gmail.com>
2013-11-22 12:07:59 -0600

    Gonna move commonly tweaked values into a separate configuration file.

---
+++ config.js
@@ -0,0 +1,13 @@
+CONFIG = (function () {
+	'use strict';
+	
+	return {
+		timeScale: 1,
+		paddleWidth: 80,
+		ballRadius: 12,
+		ballSpeed: 400,
+		multiplierTimeout: 3.4,
+		numCherryTokens: 5,
+		numBananaTokens: 2
+	};
+}());
